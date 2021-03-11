1bc686ed689fb3212fae5408f91a2bcc3e9992ed
LiangYu <liangyulin0727@gmail.com>
2021-03-11 12:41:52 -0800

    added shop page

--- src/App.js
+++ src/App.js
@@ -1,12 +1,24 @@
 import React from "react";
+import { Route, Switch } from "react-router-dom";
 import "./App.css";
 
-import HomePage from "./homepage.component";
+import HomePage from "./pages/homepage/homepage.component";
 
+// const TopicDetail = (props) => {
+//   return (
+//     <div>
+//       <h1>Topic detail page: {props.match.params.topicId}</h1>
+//     </div>
+//   );
+// };
 function App() {
   return (
     <div>
-      <HomePage />
+      <Switch>
+        <Route path='/shop' component={ShopPage} />
+        <Route exact path='/' component={HomePage} />
+        {/*<Route path='/topics/:topicId' component={TopicDetail} />*/}
+      </Switch>
     </div>
   );
 }
